011d350483ad3094c184c08d669d8c9c94e10da4
fabiocarneiro <fahecs@gmail.com>
2014-12-11 11:15:01 -0200

    add tests

---
+++ test/RepositoryManagerTest/Model/RepositoryTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ZFBrasil\RepositoryManagerTest\Model;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use ZFBrasil\RepositoryManager\Model\Repository;
+use ZFBrasil\RepositoryManager\Model\RepositoryInterface;
+
+/**
+ * @author  Fábio Carneiro <fahecs@gmail.com>
+ * @license MIT
+ */
+class RepositoryTest extends TestCase
+{
+    public function testValidTypeReturnRepositoryInstance()
+    {
+        $this->assertInstanceOf(RepositoryInterface::class, Repository::VCS('http://github.com'));
+    }
+}
+ 
