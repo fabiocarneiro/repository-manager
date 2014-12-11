126918e1838e55b832cd61484c7667911e94d573
fabiocarneiro <fahecs@gmail.com>
2014-12-11 13:16:36 -0200

    new test for type model

---
+++ test/RepositoryManagerTest/Model/TypeTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ZFBrasil\RepositoryManagerTest\Model;
+
+
+/**
+ * @author Fábio Carneiro <fahecs@gmail.com>
+ * @license proprietary
+ */
+ class TypeTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
