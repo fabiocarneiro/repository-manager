9c6ff71ac50b687a9db464a65e6b215d2d0058da
fabiocarneiro <fahecs@gmail.com>
2014-12-11 11:50:30 -0200

    use single quotes

--- Module.php
+++ Module.php
@@ -15,6 +15,6 @@ class Module implements ConfigProviderInterface
      */
     public function getConfig()
     {
-        return require __DIR__ . "/../config/module.config.php";
+        return require __DIR__ . '/../config/module.config.php';
     }
 }
